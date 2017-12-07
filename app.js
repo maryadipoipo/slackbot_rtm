770096bfb0d10b18f82400c064afeb3805421709
Maryadi <maryadi@astrnt.co>
2017-12-07 15:00:27 +0700

    Done testing CLIENT_EVENT

--- app.js
+++ app.js
@@ -12,14 +12,20 @@ var rtm = new rtmClient(process.env.SLACK_API_TOKEN);
 /*** Listening RTM EVENT from SLACK ***/
 let channel;
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-  for (const c of rtmStartData.channels) {
-      if (c.is_member && c.name ==='jamiestestchannel') { channel = c.id }
+  for (const c of rtmStartData.channels) {	  
+      if (c.is_member && c.name ==='random') { 
+		channel = c.id ;		
+	  }
   }
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
 });
 /***************************************/
 
-
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {	
+   if(channel != 'undefined') {
+		rtm.sendMessage("Hello! Semuanya..ehehe...Sy baru OL lagi nih... :)", channel);
+   }
+});
 
 rtm.start();
 
