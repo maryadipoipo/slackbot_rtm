d4982a4a997651312ef9508bd586dd4c45eb94e0
Maryadi <maryadi@astrnt.co>
2017-12-10 09:13:31 +0700

    add new invited channels and checking it

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
 var rtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var my_env = require('node-env-file');
+var mongo_poipo = require('./mongodb_poipo');
+
+
 
 /*** Load ENV ***/
 my_env(__dirname+'/.env');
 //console.log(process.env.SLACK_API_TOKEN);
 /*********************/
 
+
 var rtm = new rtmClient(process.env.SLACK_API_TOKEN);
 
 /*** Listening RTM EVENT from SLACK ***/
-let channel;
+let channel_id;
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-  for (const c of rtmStartData.channels) {	  
-      if (c.is_member && c.name ==='random') { 
-		channel = c.id ;		
-	  }
+  for (const channel of rtmStartData.channels) {
+      if (channel.is_member && !channel.is_archived) {
+  		  channel_id = channel.id
+        mongo_poipo.check_invited_channels(channel);
+  	  }
   }
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
+  //console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
 });
 /***************************************/
 
-rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {	
-   if(channel != 'undefined') {
-		rtm.sendMessage("Hello! Semuanya..ehehe...Sy baru OL lagi nih... :)", channel);
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+   if(channel_id != 'undefined') {
+		rtm.sendMessage("Hello! Semuanya..ehehe...Sy baru OL lagi nih... :)", channel_id);
    }
 });
 
+
+// /*** For detecting message in channel ***/
+// rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+//     console.log(message);
+// });
+
+// /*** For detecting DM ***/
+// rtm.on(RTM_EVENTS.IM_OPEN, function(message) {
+//     console.log(message);
+// });
+
+
 rtm.start();
+
+
+//mongo_poipo.get_remain_karma_points(1);
 
 
--- mongodb_poipo.js
+++ mongodb_poipo.js
@@ -1,5 +1,7 @@
+var my_env = require('node-env-file');
 var MongoClient = require('mongodb').MongoClient;
 
+my_env(__dirname+'/.env');
 var url = "mongodb://"
           +process.env.MONGODB_USER
           +":"
@@ -7,20 +9,58 @@ var url = "mongodb://"
           +"@ds129936.mlab.com:29936/"
           +process.env.MONGODB_DATABASE;
 
+function add_new_invited_channel (dbase, new_obj_channel) {
+    var new_channel_data = {
+        slack_team_id : 0, // not yet used
+        slack_channel_id : new_obj_channel.id,
+        slack_channel_name : new_obj_channel.name,
+        slack_channel_member_ids : new_obj_channel.members,
+        is_deleted: false
+    };
+    dbase.collection(process.env.MONGODB_COLLECTION_INVITED_CHANNELS)
+        .insertOne(new_channel_data, function(err, res) { //insertOne buat 1 object
+              if (err) throw err;
+              console.log("new invited channel inserted successfully : "+new_obj_channel.id);
+        });
+}
 
 module.exports = {
-    get_remain_karma_points: function(m_user_id) {
+    check_invited_channels: function(obj_channel) {
         MongoClient.connect(url, function(err, db) {
             if (err) throw err;
 
             var dbase = db.db(process.env.MONGODB_DATABASE);
-            var query = { user_id: m_user_id };
+            var query = { slack_channel_id: obj_channel.id };
 
-            dbase.collection("karma_points").find(query).toArray(function(err, result) {
+            dbase.collection(process.env.MONGODB_COLLECTION_INVITED_CHANNELS)
+                .find(query)
+                .toArray(function(err, result) {
+                    if(result.length < 1) {
+                        /** Add this new invited channel **/
+                        add_new_invited_channel(dbase, obj_channel);
+                        db.close();
+                    }
+                });
+
+
+        })
+    },
+    get_remain_karma_points: function(i_user_id) {
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+
+            var dbase = db.db(process.env.MONGODB_DATABASE);
+            var query = { user_id: i_user_id };
+
+            dbase.collection(process.env.MONGODB_COLLECTION).find(query).toArray(function(err, result) {
                 console.log(" remain karma points : ");
                 console.log(result);
             });
-        }
+        })
     },
+
+    show_top_10:function() {
+
+    }
 
 }
