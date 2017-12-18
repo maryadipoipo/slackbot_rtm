b93f206f4d044986432ee2df945398659dda5cad
Maryadi <maryadi@astrnt.co>
2017-12-18 11:10:03 +0700

    testing

--- app.js
+++ app.js
@@ -189,14 +189,15 @@ app.listen(port, function () {
 app.get('/home', function (req, res) {
   //console.log(req);
   res.writeHead(200, {'Content-TYpe': 'text/html'});
-   res.write(`<h1>Hellow There...</h1><br /><br />Thanks for visiting... :) <br><br>
-        <a href="https://slack.com/oauth/authorize?scope=bot&client_id=281187440289.284693862498">
-          <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
-            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,
-            https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
-        </a>
+   // res.write(`<h1>Hellow There...</h1><br /><br />Thanks for visiting... :) <br><br>
+   //      <a href="https://slack.com/oauth/authorize?scope=bot&client_id=281187440289.284693862498">
+   //        <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
+   //          srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,
+   //          https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+   //      </a>
 
-    `);
+   //  `);
+   res.write("Hellow write");
     //res.status(200).send('Hello world!');
     res.end();
   }
