c648fcad97d5c41a4b14623c3f5bf63b6a49a3cc
Maryadi <maryadi@astrnt.co>
2017-12-18 11:13:52 +0700

    simple

--- app.js
+++ app.js
@@ -198,15 +198,15 @@ app.get('/home', function (req, res) {
 
    //  `);
    //res.write("Hellow write");
-    //res.status(200).send('Hello world!');
-    res.status(200).send(`<h1>Hellow There...</h1><br /><br />Thanks for visiting... :) <br><br>
-         <a href="https://slack.com/oauth/authorize?scope=bot&client_id=281187440289.284693862498">
-           <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
-             srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,
-             https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
-        </a>
+    res.status(200).send('Hello world!');
+    // res.status(200).send(`<h1>Hellow There...</h1><br /><br />Thanks for visiting... :) <br><br>
+    //      <a href="https://slack.com/oauth/authorize?scope=bot&client_id=281187440289.284693862498">
+    //        <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
+    //          srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,
+    //          https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+    //     </a>
 
-     `);
+    //  `);
     res.end();
   }
 );
