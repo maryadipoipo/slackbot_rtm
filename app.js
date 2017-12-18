8fe3f6bd6278e47cff9fb24569f78c89f583abae
Maryadi <maryadi@astrnt.co>
2017-12-18 10:16:32 +0700

    add homepage

--- app.js
+++ app.js
@@ -171,6 +171,39 @@ rtm.on(RTM_EVENTS.BOT_ADDED, function(message) {
 
 
 
+/******************* EXPRESS *********************/
+var app = express();
+var port = process.env.PORT || 1777;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//Starting server
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
+
+
+// test route
+app.get('/', function (req, res) {
+  //console.log(req);
+  res.writeHead(200, {'Content-TYpe': 'text/html'});
+   res.write(`<h1>Hellow There...</h1><br /><br />Thanks for visiting... :) <br><br>
+        <a href="https://slack.com/oauth/authorize?scope=bot&client_id=281187440289.284693862498">
+          <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
+            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,
+            https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+        </a>
+
+    `);
+    //res.status(200).send('Hello world!');
+    res.end();
+  }
+);
+
+
+
 
 
 
